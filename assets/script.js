cf709ffeed52a6c9df8c4f0e9db2e76af2ba3635
Jacek <jacek@webdev.com>
2019-10-16 00:43:04 +0200

    Create masonry portfolio gallery

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const macyContainer = new Macy({
+    container: '.macy__container',
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+        400: 2,
+        700: 3,
+        1100: 4
+    },
+    margin: {
+        x: 20,
+        y: 20
+    }
+});
