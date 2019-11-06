6d1813af8048f7f5292664142e0966b11e071a1e
Jacek <jacek@webdev.com>
2019-11-06 21:58:28 +0100

    Working navigation with hooks and working item--active class

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,75 @@ const macyContainer = new Macy({
 
 const scrollNav = new SmoothScroll('a[class*="item__link"]');
 const scrollLogo = new SmoothScroll('a[href*="#header"]');
+let sections;
+window.onload = () => {
+    sections = 
+    [
+        { "sectionName": "header", "offsetTop": document.querySelector('#header').offsetTop },
+        { "sectionName": "offer", "offsetTop": document.querySelector('#offer').offsetTop },
+        { "sectionName": "portfolio", "offsetTop": document.querySelector('#portfolio').offsetTop },
+        { "sectionName": "pricing", "offsetTop": document.querySelector('#pricing').offsetTop },
+        { "sectionName": "contact", "offsetTop": document.querySelector('#contact').offsetTop }
+    ];
+}
+window.addEventListener('resize', () => {
+    sections = 
+    [
+        { "sectionName": "header", "offsetTop": document.querySelector('#header').offsetTop },
+        { "sectionName": "offer", "offsetTop": document.querySelector('#offer').offsetTop },
+        { "sectionName": "portfolio", "offsetTop": document.querySelector('#portfolio').offsetTop },
+        { "sectionName": "pricing", "offsetTop": document.querySelector('#pricing').offsetTop },
+        { "sectionName": "contact", "offsetTop": document.querySelector('#contact').offsetTop }
+    ];
+});
 
+document.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset;
+    let i = 0;
+    console.log(i);
+    
+    scrollTop < sections[1]["offsetTop"] ? (
+        sections.forEach(() => {
+            document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+            i++;
+        }),i=0)
+    : (
+        scrollTop >= sections[1]["offsetTop"] && scrollTop < sections[2]["offsetTop"] ? (
+            sections.forEach(() => {
+                document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+                i++;
+            }),i=0,
+            document.querySelector('a[href="#'+sections[1]['sectionName']+'"]').classList.add("item__link--active"))
+        : (
+            scrollTop >= sections[2]["offsetTop"] && scrollTop < sections[3]["offsetTop"] ? (
+                sections.forEach(() => {
+                    document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+                    i++;
+                }),i=0,
+                document.querySelector('a[href="#'+sections[2]['sectionName']+'"]').classList.add("item__link--active"))
+            : (
+                scrollTop >= sections[3]["offsetTop"] && scrollTop < sections[4]["offsetTop"] ? (
+                    sections.forEach(() => {
+                        document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+                        i++;
+                    }),i=0,
+                    document.querySelector('a[href="#'+sections[3]['sectionName']+'"]').classList.add("item__link--active"))
+                : (
+                    scrollTop >= sections[4]["offsetTop"] ? (
+                        sections.forEach(() => {
+                            document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+                            i++;
+                        }),i=0,
+                        document.querySelector('a[href="#'+sections[4]['sectionName']+'"]').classList.add("item__link--active"))
+                    : (
+                        sections.forEach(() => {
+                            document.querySelector('a[href="#'+sections[i]['sectionName']+'"]').classList.remove("item__link--active");
+                            i++;i=0;
+                        })
+                    )
+                )
+            )
+        )
+    )
 
-// const nav = document.querySelector('.nav');
-// window.addEventListener('scroll', () => {
-//     (window.scrollY > 0) ? nav.classList.add('nav--scroll');
-// });
+});
